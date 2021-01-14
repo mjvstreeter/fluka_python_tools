7687ef78328246edba476428de86ff899f699f82
Matthew Streeter <22773109+mjvstreeter@users.noreply.github.com>
2021-01-14 20:47:25 +0000

    added diagnostic tools

--- fluka_python_tools/diagnostics.py
+++ fluka_python_tools/diagnostics.py
@@ -24,3 +24,58 @@ def propagate_beam(p_data,z,same_time=False):
     p_data_out[:,6] = p_data[:,6] + dt*b*p_data_out[:,10]*c*1e2
     p_data_out[:,7] = p_data[:,7] + dt
     return p_data_out
+
+
+def radial_beam_hist(p_data,r_axis=None,z_axis=None,weight=None):
+    if r_axis is None:
+        r_axis = 100
+    if z_axis is None:
+        z_axis = 100
+    
+    e_r = np.sqrt(p_data[:,4]**2+p_data[:,5]**2) # cm
+    e_z = p_data[:,6]
+    H,r_axis,z_axis = np.histogram2d(e_r, e_z,bins=(r_axis,z_axis), #, bins=(100,100), #
+                                 range=None, normed=None, weights=np.ones_like(e_r)*weight, density=None)
+
+    dr = np.mean(np.diff(r_axis))
+    dz = np.mean(np.diff(z_axis))
+    r_area = pi*(r_axis[1:]**2-r_axis[:-1]**2).reshape(1,-1)
+    n_e_zr = (H.T)/(r_area*dz) # particles per cm3
+
+    return n_e_zr, r_axis, z_axis
+
+def sim_beam_dist(p_data_in,r_axis,z_axis,p_weight=1,
+                  primary_sigma_r = 0.1, primary_div= 1e-3,primary_sigma_t = 10e-15,
+                  N_rand = 10,r_max = 300e-4):
+
+    p_data = np.copy(p_data_in)
+    N_p = np.size(p_data,axis=0)
+    z_min = np.min(z_axis)
+    z_max = np.max(z_axis)
+    g_axis = np.linspace(0,np.max(p_data[:,11]),num=400)
+    n_g = 0 
+    n_zr_sum = 0
+    for n in range(N_rand):
+        # random jiggle transverse position and divergence
+        x_rand = np.random.randn(N_p)
+        p_data[:,4] = p_data[:,4] + x_rand*primary_sigma_r
+        p_data[:,8] = p_data[:,8] + x_rand*primary_div
+        y_rand = np.random.randn(N_p)
+        p_data[:,5] = p_data[:,5] + y_rand*primary_sigma_r
+        p_data[:,9] = p_data[:,9] + y_rand*primary_div
+        # random jiggle longitudinal position
+        z_rand = np.random.randn(N_p)
+        p_data[:,6] = p_data[:,6] + z_rand*primary_sigma_t*c*1e2
+
+        n_zr,r,z = radial_beam_hist(p_data,r_axis=r_axis,z_axis=z_axis,weight=p_weight/N_rand)
+        n_zr_sum = n_zr_sum+n_zr
+        
+        e_r = np.sqrt(p_data[:,4]**2+p_data[:,5]**2) # cm
+        e_z = p_data[:,6]
+        
+        g_sel = (e_r<=r_max)*(e_z>=z_min)*(e_z<=z_max)
+        H,Hx = np.histogram(p_data[g_sel,11],bins=g_axis,normed=False)
+        n_g=n_g+H*p_weight/N_rand
+    
+    part_dist = dict(n_zr= n_zr,r=r,z=z, g_axis=g_axis,n_g=n_g)
+    return part_dist
--- fluka_python_tools/io.py
+++ fluka_python_tools/io.py
@@ -1,25 +1,33 @@
 import numpy as np
 from scipy.constants import m_e, e, c, pi
+import re
+from physics_tools.general_tools import load_object
 
-# def read_usrbin_dat(file_path):
+def read_usrbin_dat(file_path):
 
-#     with open(file_path, "r") as f:
-#         f_lines = f.readlines()
-#     x_edges = []
-#     Nxc = []
-#     for s in f_lines[2:5]:
-#         match_number = re.compile('-?\ *[0-9]+\.?[0-9]*(?:[Ee][+-]\ *-?\ *[0-9]+)?')
-#         nums = [float(x) for x in re.findall(match_number, s)]
-#         x_edges.append(np.linspace(nums[0],nums[1],num=int(nums[2])+1,endpoint=True))
-#         Nxc.append(int(nums[2]))
-#     data =[]
-#     n_l = []
-#     for s in f_lines[9:80009]:
-#         nums = [float(x) for x in re.findall(match_number, s)]
-#         n_l.append(len(nums))
-#         data.append(nums)
-#     data = np.array(data)
-#     data = data.reshape((Nxc[2],Nxc[0])).T;
+    with open(file_path, "r") as f:
+        f_lines = f.readlines()
+    x_edges = []
+    Nxc = []
+    for s in f_lines[2:5]:
+        match_number = re.compile('-?\ *[0-9]+\.?[0-9]*(?:[Ee][+-]\ *-?\ *[0-9]+)?')
+        nums = [float(x) for x in re.findall(match_number, s)]
+        x_edges.append(np.linspace(nums[0],nums[1],num=int(nums[2])+1,endpoint=True))
+        Nxc.append(int(nums[2]))
+
+    full_data = []
+    N_group = int(Nxc[0]*Nxc[2]/10)
+    for iy in range(Nxc[1]):
+        data =[]
+        n_l = []
+        for s in f_lines[(9+iy*N_group):(9+((iy+1)*N_group))]:
+            nums = [float(x) for x in re.findall(match_number, s)]
+            n_l.append(len(nums))
+            data.append(nums)
+        data = np.array(data)
+        data = data.reshape((Nxc[2],Nxc[0])).T
+        full_data.append(data)
+    return full_data, x_edges
 
 
 def load_geometry_elements(file_path):
@@ -27,6 +35,7 @@ def load_geometry_elements(file_path):
         f_lines = f.readlines()
     geo_lines=[]
     geo_flag = False
+    match_number = re.compile(' -?\ *[0-9]+\.?[0-9]*(?:[Ee][+-]\ *-?\ *[0-9]+)?')
 
     for line in f_lines:    
         if line.startswith('END'):
@@ -37,4 +46,65 @@ def load_geometry_elements(file_path):
             
         if line.startswith('GEOBEGIN'):
             geo_flag = True
-    return geo_lines
+    geo_list = []
+    for line in geo_lines:
+        nums = [float(x) for x in re.findall(match_number, line)]
+        geo_list.append(
+            dict(
+                Type=line.split()[0],
+                Name=line.split()[1],
+                x=nums
+                ))
+
+    
+    return geo_list, geo_lines
+
+def get_particle_data(data_array_file,species_list = [3,4,7],
+                from_select = [5], to_select = None,
+                z_lims = None, pz_lims = [0,None]):
+    
+    data_array  = load_object(data_array_file)
+    p_list = []
+    for s in species_list:
+        p_sel = data_array.astype(int)[:,2]==int(s)
+
+        if from_select is not None:
+            f_sel = p_sel*0.0
+            for f in from_select:
+                f_sel += data_array.astype(int)[:,0]==int(f)
+            p_sel = p_sel*f_sel
+
+        if to_select is not None:
+            f_sel = p_sel*0.0
+            for f in from_select:
+                f_sel += data_array.astype(int)[:,0]==int(f)
+            p_sel = p_sel*f_sel
+
+        if z_lims is not None:
+            f_sel = np.ones_like(p_sel)
+            if z_lims[0] is not None:
+                f_sel = f_sel*(data_array[:,6]>=z_lims[0])
+            if z_lims[1] is not None:
+                f_sel = f_sel*(data_array[:,6]<=z_lims[1])
+            p_sel = p_sel*f_sel
+
+        if pz_lims is not None:
+            f_sel = np.ones_like(p_sel)
+            if pz_lims[0] is not None:
+                f_sel = f_sel*(data_array[:,11]>=pz_lims[0])
+            if pz_lims[1] is not None:
+                f_sel = f_sel*(data_array[:,11]<=pz_lims[1])
+            p_sel = p_sel*f_sel
+
+        p_list.append(data_array[p_sel>0,:])
+    return p_list
+
+def get_primary_energy(inp_file_path):
+    with open(inp_file_path, "r") as f:
+        text = f.readlines()
+  
+    s = [x for x in text if x.startswith('BEAM ')]
+    primary_energy = float(s[0].strip().split()[1])*-1
+    return primary_energy
+
+
