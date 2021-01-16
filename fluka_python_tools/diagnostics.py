4844af8740a824b71e57a6c3b6349010738c7d64
Matthew Streeter <22773109+mjvstreeter@users.noreply.github.com>
2021-01-16 17:34:24 +0000

    added radial spectrum
    also added pickle5 load support

--- fluka_python_tools/diagnostics.py
+++ fluka_python_tools/diagnostics.py
@@ -44,17 +44,37 @@ def radial_beam_hist(p_data,r_axis=None,z_axis=None,weight=None):
 
     return n_e_zr, r_axis, z_axis
 
-def sim_beam_dist(p_data_in,r_axis,z_axis,p_weight=1,
+def radial_spectrum(p_data,r_axis=None,E_axis=None,weight=None):
+    if r_axis is None:
+        r_axis = 100
+    if E_axis is None:
+        E_axis = 100
+    
+    e_r = np.sqrt(p_data[:,4]**2+p_data[:,5]**2) # cm
+    e_pz = p_data[:,11]
+    H,r_axis,E_axis = np.histogram2d(e_r, e_pz,bins=(r_axis,E_axis), #, bins=(100,100), #
+                                 range=None, normed=None, weights=np.ones_like(e_r)*weight, density=None)
+
+    dr = np.mean(np.diff(r_axis))
+    dE = (np.diff(E_axis))[:,np.newaxis]
+    r_area = pi*(r_axis[1:]**2-r_axis[:-1]**2).reshape(1,-1)
+    n_e_Er = (H.T)/(r_area*dE) # particles per cm3
+
+    return n_e_Er, r_axis, E_axis
+
+def sim_beam_dist(p_data_in,r_axis,z_axis,p_weight=1,primary_energy=1,
                   primary_sigma_r = 0.1, primary_div= 1e-3,primary_sigma_t = 10e-15,
                   N_rand = 10,r_max = 300e-4):
 
     p_data = np.copy(p_data_in)
     N_p = np.size(p_data,axis=0)
-    z_min = np.min(z_axis)
-    z_max = np.max(z_axis)
-    g_axis = np.linspace(0,np.max(p_data[:,11]),num=400)
-    n_g = 0 
+
+    g_axis = np.linspace(0,primary_energy,num=400)
+    
+    E_MeV_log = np.logspace(-3,0,num=100,endpoint=True)*primary_energy
+
     n_zr_sum = 0
+    n_e_Er_sum = 0
     for n in range(N_rand):
         # random jiggle transverse position and divergence
         x_rand = np.random.randn(N_p)
@@ -73,9 +93,9 @@ def sim_beam_dist(p_data_in,r_axis,z_axis,p_weight=1,
         e_r = np.sqrt(p_data[:,4]**2+p_data[:,5]**2) # cm
         e_z = p_data[:,6]
         
-        g_sel = (e_r<=r_max)*(e_z>=z_min)*(e_z<=z_max)
-        H,Hx = np.histogram(p_data[g_sel,11],bins=g_axis,normed=False)
-        n_g=n_g+H*p_weight/N_rand
+        n_e_Er, r, E_axis = radial_spectrum(p_data,r_axis=r_axis,E_axis=E_MeV_log,weight=p_weight/N_rand)
+
+        n_e_Er_sum = n_e_Er_sum+n_e_Er
     
-    part_dist = dict(n_zr= n_zr,r=r,z=z, g_axis=g_axis,n_g=n_g)
+    part_dist = dict(n_zr= n_zr,r=r,z=z, E_MeV_log=E_MeV_log,n_Er=n_e_Er_sum)
     return part_dist
